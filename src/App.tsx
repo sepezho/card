df124cf46f4cf60c0ec94cbeb83ef362a6508ec0
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-02-14 19:45:10 +0100

    add stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          sepezho
+          <br />
+          ------------
+          <br />
+          fullstack: ts & func
+          <br />
+          co-founder: tonana & toncells & evaa & raven
+          <br />
+          my wallet <a href='https://tonscan.org/address/EQBUL9aM4IKaBerYv8hZP1qLgRnEPnV2DYswO0aGsywZHsVW'>EQB...sVW</a>
+          <br />
+          my <a href='https://github.com/sepezho/cv/blob/main/cv.pdf'>cv</a>
+          <br />
+          ------------
+          <br />
+          old <a href='https://old.sepezho.com'>site</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
